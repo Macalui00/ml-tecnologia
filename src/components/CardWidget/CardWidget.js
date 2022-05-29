db7dbb33fdd17204d982151a6edf810a815b3f8c
Macalui00 <macarenalui@gmail.com>
2022-05-29 18:08:45 -0300

    Modificaciones de estilo del Card Widget

--- src/components/CardWidget/CardWidget.js
+++ src/components/CardWidget/CardWidget.js
@@ -4,8 +4,11 @@ import { BsFillCartFill } from "react-icons/bs";
 
 const CardWidget = () => {
     return (
-        <Card className='cardW'>
-            <Card.Body className='d-flex p-2 m-0'><BsFillCartFill color='white' className='mb-0'/> <h4 className='mb-0 ms-2'>4</h4></Card.Body>
+        <Card className='cardW mt-1'>
+            <Card.Body className='d-flex p-2 m-0'>
+                <BsFillCartFill color='white' className='mb-0'/>
+                <h4 className='mb-0 ms-2 text-warning'>4</h4>
+            </Card.Body>
         </Card>
     )
 }
