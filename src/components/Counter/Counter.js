3716ecc6f5a543ed5c7b472dbc04704c1fac6bad
Macalui00 <macarenalui@gmail.com>
2022-05-29 10:20:04 -0300

    Counter

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,48 @@
+import './Counter.css';
+import { useState, useEffect } from "react";
+import { InputGroup, Button, FormControl } from "react-bootstrap";
+import { BsPlus, BsDash } from "react-icons/bs";
+
+const Counter = ({stock = 4}) => {
+
+    let [stock2, setStockC2] = useState(stock);
+    let [contador, setContador] = useState(1);
+
+    const incrementar = () => {
+        if (stock > contador) {
+            setStockC2(stock2 - 1);
+            setContador(contador + 1);
+        }
+    }
+
+    const decrementar = () => {
+        if (contador > 1) {
+            setStockC2(stock2 + 1);
+            setContador(contador - 1);
+        }
+    }
+
+    return (
+        <div className="mt-3">
+            <h4 className="text-dark mb-3">Agregar elemento al carrito</h4>
+            <InputGroup className="mb-3">
+                <Button variant="primary" id="button-decrementar" className="pt-0" onClick={decrementar}>
+                    <BsDash color='white'/>
+                </Button>
+                <FormControl className="text-center" 
+                    aria-label="Incrementador/decrementador de cantidad de items"
+                    aria-describedby="basic-addon1"
+                    value={contador}
+                />
+                <Button variant="primary" id="button-incrementar" className="pt-0" onClick={incrementar}>
+                    <BsPlus color='white'/>
+                </Button>
+            </InputGroup>
+            <Button variant="primary" id="button-agregarCarrito" className="pt-1">
+                Agregar al Carrito
+            </Button>
+        </div>
+    )
+}
+
+export default Counter
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,16 +1,34 @@
 import './ItemListContainer.css';
+import { Card, Button } from 'react-bootstrap';
+import Counter from '../Counter/Counter';
+import { useState } from 'react';
 
 const ItemListContainer = ({producto}) => {
+    const {nombre, tipo, marca, memoria} = producto;
+
+    const [mostrar, setMostrar] = useState(true);
+    const mostrarCounter = () => {
+        setMostrar(!mostrar)
+    }
     return (
         <section className='productos mt-5'>
             <h2 className='fw-bold'>Nuestros productos</h2>
             <hr/>
-            <ul className='m-0 p-0'>
-                <li><p className='fw-bold text-warning'>{producto.nombre}</p></li>
-                <li><p className='detalle'>Tipo: {producto.tipo}</p></li>
-                <li><p className='detalle'>Marca: {producto.marca}</p></li>
-                <li><p className='detalle'>Memoria: {producto.memoria}</p></li>
-            </ul>
+            <Card style={{ width: '18rem' }}>
+                <Card.Header className='card-header'><Card.Title className='fw-bold text-warning mb-0'>{nombre}</Card.Title></Card.Header>
+                <Card.Body>
+                    <ul className='m-0 p-0 text-dark'>
+                        <li><p className='detalle'>Tipo: {tipo}</p></li>
+                        <li><p className='detalle'>Marca: {marca}</p></li>
+                        <li><p className='detalle'>Memoria: {memoria}</p></li>
+                    </ul>
+                    <Button variant="primary" onClick={mostrarCounter}>Comprar</Button>
+                    {
+                        mostrar ? <Counter stock={1}/> : <div></div>
+                    }
+                </Card.Body>
+            </Card>
+            
         </section>
     )
 }
