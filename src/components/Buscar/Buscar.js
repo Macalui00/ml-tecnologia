50dfb67d0945c7edd47a80f0a7609129078283f3
Macalui00 <macarenalui@gmail.com>
2022-06-28 18:30:01 -0300

    Agregado de Buscador

---
+++ src/components/Buscar/Buscar.js
@@ -0,0 +1,36 @@
+import { Form, FormControl, Button, Container, Row } from "react-bootstrap";
+
+const Buscar = ({buscarNombre, setBuscarNombre}) => {
+
+    return (
+        <Container className="container-sm container-fluid mb-4">
+            <Form className="d-flex text-center justify-content-center">
+                    <FormControl
+                    type="search"
+                    placeholder="Buscar Producto"
+                    className="me-2"
+                    aria-label="Buscar"
+                    value={buscarNombre}
+                    onChange={(event) => {
+                        setBuscarNombre(event.target.value);
+                        console.log(event.target.value)
+                    }}
+                    style={{width:'500px', fontSize:'20px'}}
+                    />
+                    {/* <Button 
+                        variant="btn btn-success"
+                        className="fw-bold fs-5" 
+                        onClick={(event) => {
+                            setBuscarNombre(event.target.value);
+                            console.log(event.target.value)
+                        }
+                        }
+                    >
+                            Buscar
+                    </Button> */}
+            </Form>         
+        </Container>
+    )
+}
+
+export default Buscar
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,20 +2,36 @@ import './ItemListContainer.css';
 import { Spinner } from 'react-bootstrap';
 import ItemList from '../ItemList.js/ItemList';
 import { useProductos } from './useProductos';
+import { useState } from "react";
+import Buscar from '../Buscar/Buscar';
 
 const ItemListContainer = () => {
+    
+    const [buscarNombre, setBuscarNombre] = useState("");
 
     const { items, loading } = useProductos()
-
+    
     return (
-        <section className='productos mt-5'>
+        <section className='productos mt-3'>
             {
                 loading
                 ?   <Spinner animation="border" role="status">
                         <span className="visually-hidden">Loading...</span>
                     </Spinner>
                 : 
-                <ItemList items={items}/>
+                <>
+                    <Buscar buscarNombre={buscarNombre} setBuscarNombre={setBuscarNombre}/>
+                    <ItemList items={items.filter(
+                                        (item) =>{
+                                            if (buscarNombre === ""){
+                                                return item
+                                            } else if (item.nombre.toLowerCase().includes(buscarNombre.toLowerCase())){
+                                                return item
+                                            }
+                                        }
+                                    )
+                    }/>
+                </>
             }
 
         </section>
