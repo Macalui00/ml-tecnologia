8605f3bff9324f544a5962678e44e30a9282b920
Macalui00 <macarenalui@gmail.com>
2022-07-09 10:36:17 -0300

    Armado de componente marca, modificación del componente banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,8 @@
 
-const PublicRoutes = () => {
+const Banner = () => {
 
     return(
         <h1 className='mt-5 text-warning font-Bungee' style={{fontSize: '35px'}}>Tu lugar favorito para comprar tecnología</h1>
     )
 }
-export default PublicRoutes
+export default Banner
---
+++ src/components/Marca/Marca.js
@@ -0,0 +1,8 @@
+
+const Marca = () => {
+
+    return(
+        <h1 className= 'text-decoration-none fw-bold text-warning font-Bungee'>ML Tecnología</h1>
+    )
+}
+export default Marca
